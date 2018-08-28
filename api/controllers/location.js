8f7ce4bc247079d044e78ffb8eeb36cd08c75e42
OlmeGMS <olme_gms@hotmail.com>
2018-08-28 16:00:28 -0500

    "inicio de map"

--- api/controllers/location.js
+++ api/controllers/location.js
@@ -23,7 +23,47 @@ function getLocation(req, res)
   });
 }
 
+function getLocations(req, res)
+{
+  if (req.params.page) {
+    var page = req.params.page;
+  }else {
+    var page = 1;
+  }
+
+  var itemsPerPage = 3;
+
+  Localidad.find().sort('location').paginate(page, itemsPerPage, function(err, locations, tola){
+    if (err) {
+      res.status(500).send({message: 'Error en la petición'});
+    }else {
+      if(!locations){
+        res.status(404).send({message: 'No hay localidads !!'});
+      }else{
+        res.status(200).send({locations: locations});
+      }
+    }
+  });
+}
+
+function getListLocations(req, res)
+{
+  Localidad.find({}, function(err, locations){
+    if (err) {
+      res.status(500).send({message: 'Error en la petición'});
+    }else {
+      if (!locations) {
+        res.status(404).send({message: 'No hay localidades !!'});
+      }else {
+        res.status(200).send({locations: locations});
+      }
+    }
+  });
+}
+
 
 module.exports = {
-  getLocation
+  getLocation,
+  getLocations,
+  getListLocations
 }
